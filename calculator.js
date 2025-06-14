347d5a8ba55e84f458b30751910e48c18ed3b251
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-06-14 07:51:49 +0100

    created a calculator

---
+++ calculator.js
@@ -0,0 +1,42 @@
+// Arithmetic Functions
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  if (number2 === 0) {
+    return "Cannot divide by zero";
+  }
+  return number1 / number2;
+}
+
+function calculate(operationFn) {
+  const number1 = parseFloat(document.getElementById("number1").value) || 0;
+  const number2 = parseFloat(document.getElementById("number2").value) || 0;
+  const result = operationFn(number1, number2);
+  document.getElementById("calculation-result").textContent = result;
+}
+
+document.getElementById("add").addEventListener("click", function () {
+  calculate(add);
+});
+
+document.getElementById("subtract").addEventListener("click", function () {
+  calculate(subtract);
+});
+
+document.getElementById("multiply").addEventListener("click", function () {
+  calculate(multiply);
+});
+
+document.getElementById("divide").addEventListener("click", function () {
+  calculate(divide);
+});
